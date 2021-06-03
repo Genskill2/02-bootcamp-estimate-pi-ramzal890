ba042a302c308c55eefefbe56ef46d696c9daa9a
ramzal890 <81103003+ramzal890@users.noreply.github.com>
2021-06-03 17:41:42 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,5 +1,22 @@
 import math
 import unittest
+import random
+
+def monte_carlo(l):
+
+    circle_points = 0
+    square_points = 0
+
+    for i in range(l ** 2):
+        rand_x = random.uniform(-1, 1)
+        rand_y = random.uniform(-1, 1)
+        origin_dist = rand_x ** 2 + rand_y ** 2
+        if origin_dist <= 1:
+            circle_points += 1
+
+        square_points += 1
+        pi = 4 * circle_points / square_points
+    return pi
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
